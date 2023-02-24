6e43cb60d411ca2b41fa80af4863c1d174489b75
taica00 <taichenan00@gmail.com>
2023-02-24 14:42:02 +0800

    dummyData

---
+++ MyTripDiary/src/models/Trip.js
@@ -0,0 +1,10 @@
+export class Trip {
+    constructor(name, originName, originCoor, destName, destCoor) {
+        this.name = name;
+        this.originName = originName;
+        this.originCoor = originCoor;
+        this.destName = destName;
+        this.destCoor = destCoor;
+    }
+};
+
--- MyTripDiary/src/screens/SavedTrips.js
+++ MyTripDiary/src/screens/SavedTrips.js
@@ -1,6 +1,7 @@
 import { Ionicons } from "@expo/vector-icons";
-import { FlatList } from "react-native";
-import { Layout, TopNav } from "react-native-rapi-ui";
+import { FlatList, StyleSheet } from "react-native";
+import { Layout, Section, SectionContent, Text, TopNav } from "react-native-rapi-ui";
+import { Trip } from "../models/Trip";
 
 function SavedTrips({ navigation }) {
     return (
@@ -10,9 +11,39 @@ function SavedTrips({ navigation }) {
                 leftAction={navigation.goBack}
                 middleContent="Saved Trips"
             />
-            <FlatList />
+            <FlatList 
+                data={dummyData}
+                renderItem={renderSavedTrip}
+            />
         </Layout>
     )
 }
+
+function renderSavedTrip(SavedTrips) {
+    return (
+        <Section style={styles.section}>
+            <Text>{SavedTrips.item.name}</Text>
+            <SectionContent>
+                <Text>{SavedTrips.item.originName} ---------------------------- {SavedTrips.item.destName}</Text>
+            </SectionContent>
+        </Section>
+    )   
+}
+
+const styles = StyleSheet.create({
+    section: {
+        elevation: 16,
+        margin: 8,
+        alignItems: 'center'
+    },
+})
+
+let dummyData = [
+    new Trip('Go to school', 'Sentosa Cove', "location", 'NTU', "location"),
+    new Trip('Go home', 'NTU', "location", 'Sentosa Cove', "location"),
+    new Trip('Go to Orchard', 'Sentosa Cove', "location", 'Orchard', "location"),
+    new Trip('Go to Seans', 'Sentosa Cove', "location", 'Sean', "location"),
+    new Trip('Go back to hall', 'NTU', "location", 'Hall 5', "location")
+];
 
 export default SavedTrips;
