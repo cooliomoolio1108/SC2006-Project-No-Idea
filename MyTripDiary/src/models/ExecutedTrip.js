30b12a5d6f51072c7091395804fc0cc1bb435bee
Armaan Goel <91489796+Armaan-Goel-NTU@users.noreply.github.com>
2023-03-23 23:02:48 +0800

    Fix Descriptions

--- MyTripDiary/src/models/ExecutedTrip.js
+++ MyTripDiary/src/models/ExecutedTrip.js
@@ -1,15 +1,13 @@
 /** 
 * A constructor for the executed trip model
-* @param {Trip} trip - The trip that has been executed
 * @param {integer} timeStamp - Timestamp of trip execution in epoch seconds
 * @param {Transport} modeOfTransport - Mode of transport, as chosen by the user
 * @param {TripPrice} tripPrice - The price object for the executed trip
 * @param {integer} duration - Duration of the trip in seconds
 */
 
-export class ExecutedTrip {
-    constructor(trip, timeStamp, modeOfTransport, tripPrice, duration) {
-        this.trip = trip;
+export class ExecutedTrip extends Trip {
+    constructor(timeStamp, modeOfTransport, tripPrice, duration) {
         this.timeStamp = timeStamp;
         this.modeOfTransport = modeOfTransport;
         this.tripPrice = tripPrice;
