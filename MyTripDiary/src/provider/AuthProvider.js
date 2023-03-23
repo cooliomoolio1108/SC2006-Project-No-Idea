08bc68bdf495fcb82e2344d1dfef85f2306e0321
ZaydenBlaze <45623919+ZaydenBlaze@users.noreply.github.com>
2023-03-23 23:57:31 +0800

    Add JSDoc to AuthProvider.js

--- MyTripDiary/src/provider/AuthProvider.js
+++ MyTripDiary/src/provider/AuthProvider.js
@@ -1,7 +1,21 @@
 import React, { createContext, useState, useEffect } from "react";
 import { getAuth, onAuthStateChanged } from "firebase/auth";
 
+/**
+ * 
+ * @typedef {Object} AuthContextValue - An object containing the current user state.
+ * @property {boolean} user - The current user's authentication state.
+ * @typedef {Object} AuthProviderProps - The props for the AuthProvider component.
+ * @property {React.ReactNode} children - The children components to be wrapped by the AuthProvider.
+ * @type {React.Context<AuthContextValue>} - The authentication context object.
+ */
 const AuthContext = createContext();
+
+/** 
+ * The authentication provider component that provides authentication state and 
+ * functions to the children components.
+ * @param {AuthProviderProps} props - The props for the AuthProvider component.
+ */
 const AuthProvider = (props) => {
   const auth = getAuth();
   // user null = loading
