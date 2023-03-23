83544776aa8d3b22521a183e04a729f964023663
Armaan Goel <91489796+Armaan-Goel-NTU@users.noreply.github.com>
2023-03-23 22:54:29 +0800

    Create ExecutedTrip.js

---
+++ MyTripDiary/src/models/ExecutedTrip.js
@@ -0,0 +1,19 @@
+/** 
+* A constructor for the executed trip model
+* @param {Trip} trip - The trip that has been executed
+* @param {integer} timeStamp - Timestamp of trip execution in epoch seconds
+* @param {Transport} modeOfTransport - Mode of transport, as chosen by the user
+* @param {number} tripPrice - The estimated price of the trip
+* @param {integer} duration - Duration of the trip in seconds
+*/
+
+export class ExecutedTrip {
+    constructor(trip, timeStamp, modeOfTransport, tripPrice, duration) {
+        this.trip = trip;
+        this.timeStamp = timeStamp;
+        this.modeOfTransport = modeOfTransport;
+        this.tripPrice = tripPrice;
+        this.duration = duration;
+    }
+};
+
