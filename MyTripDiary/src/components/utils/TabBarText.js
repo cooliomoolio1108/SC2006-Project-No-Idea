12a2e29c9fc39e47163edf7f149ca233ecb9fd9c
ZaydenBlaze <45623919+ZaydenBlaze@users.noreply.github.com>
2023-03-24 00:03:26 +0800

    Add JSDoc to TabBarText.js

--- MyTripDiary/src/components/utils/TabBarText.js
+++ MyTripDiary/src/components/utils/TabBarText.js
@@ -1,5 +1,13 @@
 import React from "react";
 import { Text, themeColor, useTheme } from "react-native-rapi-ui";
+
+/**
+ * A functional component that renders a text label based on whether it is focused or not.
+ * @param {Object} props - The props for the component.
+ * @param {string} props.title - The text to display in the label.
+ * @param {boolean} props.focused - Whether the label is currently focused or not.
+ * @returns {JSX.Element} - Returns the JSX for the label component.
+ */
 export default (props) => {
   const { isDarkmode } = useTheme();
   return (
