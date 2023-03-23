544c5f74f1fc3ecdfa180d5db31828467ff33d32
ardnep <aranepal@gmail.com>
2023-03-23 21:53:52 +0800

    Settings v0

---
+++ MyTripDiary/personalisation.js
@@ -0,0 +1,6 @@
+import { StyleSheet, Text, View, SafeAreaView, Button, TouchableOpacity} from 'react-native';
+export default function personalisation() {
+    <SafeAreaView>
+        <Text>HI</Text>
+    </SafeAreaView>
+}
