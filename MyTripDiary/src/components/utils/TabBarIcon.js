4725adc9d86b44481e05d1f8804726282886cb7e
ZaydenBlaze <45623919+ZaydenBlaze@users.noreply.github.com>
2023-03-24 00:08:49 +0800

    Add JSDoc to TabBarIcon.js

--- MyTripDiary/src/components/utils/TabBarIcon.js
+++ MyTripDiary/src/components/utils/TabBarIcon.js
@@ -2,6 +2,13 @@ import React from "react";
 import { themeColor, useTheme } from "react-native-rapi-ui";
 import { Ionicons } from "@expo/vector-icons";
 
+/**
+ * A reusable component that renders an Ionicons icon.
+ * @param {Object} props - The props for the component.
+ * @param {string} props.icon - The name of the Ionicons icon to render.
+ * @param {boolean} props.focused - A boolean value that indicates whether the component is currently focused.
+ * @returns {JSX.Element} - The rendered Ionicons icon component.
+ */
 export default (props) => {
   const { isDarkmode } = useTheme();
   return (
